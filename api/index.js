1ed7045aa7472b68ceb42f6329733efa62c24a6f
Suhanii1214 <suhani.12010153@lpu.in>
2023-10-09 03:11:23 +0530

    Created test api routes

--- api/index.js
+++ api/index.js
@@ -1,5 +1,16 @@
-import { log } from "console";
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import userRouter from "./routes/user.routes.js"
+
+dotenv.config();
+
+mongoose.connect("mongodb+srv://suhanii1214:Suhani12345@cluster0.t6pvr22.mongodb.net/?retryWrites=true&w=majority")
+.then(() => {
+    console.log("Connected to MongoDB!");
+}).catch((err) => {
+    console.log(err);
+})
 
 const app = express();
 const port = 3000;
@@ -7,3 +18,5 @@ const port = 3000;
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
 })
+
+app.use("/api/user", userRouter)
